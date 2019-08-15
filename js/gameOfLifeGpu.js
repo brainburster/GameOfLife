ce4802e36252507384583dcca78f7470b9171762
ZengLiyuan <brainburster@outlook.com>
2019-08-15 17:51:44 +0800

    基本的着色器，和数据

--- js/gameOfLifeGpu.js
+++ js/gameOfLifeGpu.js
@@ -1,10 +1,53 @@
+const vsText = `
+precision lowp float;
+
+attribute vec2 position;
+attribute vec2 texCoord;
+//uniform mat4 m;
+varying vec2 uv;
+
+void main(){
+  gl_Position=vec4(position,0,1);
+  uv=texCoord;
+}
+`
+const fsText = `
+precision lowp float;
+
+varying vec2 uv;
+uniform sampler2D sampler;
+void main(){
+  gl_FragColor=texture2D(sampler,uv);
+}
+`
+
+const rect = {
+  positions: new Float32Array([
+    -1.0, -1.0,
+    1.0, -1.0,
+    1.0, 1.0,
+    -1.0, 1.0,
+  ]),
+  uvs: new Float32Array([
+    0, 1,
+    1, 1,
+    1, 0,
+    0, 0
+  ]),
+  indices: new Uint8Array([
+    0, 1, 2, 0, 2, 3
+  ]),
+  count: 6
+}
+
 class GameOfLife {
   constructor(w, h, cvsW = 800, cvsH = 600) {
+    this.delay = 16; //ms
     this.cvs = document.createElement("canvas");
     this.gl = this.cvs.getContext("webgl");
     this.cvs.width = cvsW;
     this.cvs.height = cvsH;
-    this.gl.viewport(cvsW, cvsH);
+    this.gl.viewport(0, 0, cvsW, cvsH);
     this.w = w;
     this.h = h;
     const gl = this.gl;
@@ -31,10 +74,52 @@ class GameOfLife {
         return this.texture1;
       }
     }
-    this.speed = 16; //ms
+
+    //创建着色器程序
+    const vs = gl.createShader(gl.VERTEX_SHADER);
+    const fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(vs, vsText);
+    gl.shaderSource(fs, fsText);
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+    this.program = gl.createProgram();
+    gl.attachShader(this.program, vs);
+    gl.attachShader(this.program, fs);
+    gl.linkProgram(this.program);
+    this.locPos = gl.getAttribLocation(this.program, "position");
+    this.locUv = gl.getAttribLocation(this.program, "texCoord");
+    gl.enableVertexAttribArray(this.locPos);
+    gl.enableVertexAttribArray(this.locUv);
+
+    //创建VAO
+    this.ext = gl.getExtension("OES_vertex_array_object");
+    this.vao = this.ext.createVertexArrayOES();
+    this.ext.bindVertexArrayOES(this.vao);
+    const posBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
+    gl.vertexAttribPointer(this.locPos, 2, gl.FLOAT, false, 0, 0);
+    gl.bufferData(gl.ARRAY_BUFFER, rect.positions, gl.STATIC_DRAW);
+    const uvBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.vertexAttribPointer(this.uvs, 2, gl.FLOAT, false, 0, 0);
+    gl.bufferData(gl.ARRAY_BUFFER, rect.uvs, gl.STATIC_DRAW);
+    const ebo = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, rect.indices, gl.STATIC_DRAW);
+    this.ext.bindVertexArrayOES(null);
+
   }
+
   getCanvas() {
     return this.cvs;
+  }
+
+  mouse2World(mx, my) {
+    //return {x,y};
+  }
+
+  init() {
+
   }
 
   handleInput() {
@@ -52,7 +137,6 @@ class GameOfLife {
   run() {
     let previous = (new Date()).getTime();
     let lag = 0.0;
-
     const gameloop = () => {
       const current = (new Date()).getTime();
       const elapsed = current - previous;
@@ -61,21 +145,22 @@ class GameOfLife {
 
       this.handleInput();
 
-      while (lag >= this.speed) {
+      while (lag >= this.delay) {
         this.update();
-        lag -= this.speed;
+        lag -= this.delay;
       }
 
-      this.renders();
-
+      this.render();
       requestAnimationFrame(gameloop);
     };
 
+    this.init();
     gameloop();
   }
 }
 
 window.onload = function () {
-  const game = GameOfLife();
+  const game = new GameOfLife(1024, 1024, 1024, 1024);
+  document.getElementById("game").appendChild(game.getCanvas());
   game.run();
 }
