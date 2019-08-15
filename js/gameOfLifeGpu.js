0d30acad46e162b5d4641990200ca82054e6d1e3
ZengLiyuan <brainburster@outlook.com>
2019-08-15 19:15:54 +0800

    基本算法框架

--- js/gameOfLifeGpu.js
+++ js/gameOfLifeGpu.js
@@ -16,6 +16,7 @@ precision lowp float;
 
 varying vec2 uv;
 uniform sampler2D sampler;
+
 void main(){
   gl_FragColor=texture2D(sampler,uv);
 }
@@ -29,20 +30,19 @@ const rect = {
     -1.0, 1.0,
   ]),
   uvs: new Float32Array([
-    0, 1,
-    1, 1,
+    0, 0,
     1, 0,
-    0, 0
+    1, 1,
+    0, 1
   ]),
   indices: new Uint8Array([
     0, 1, 2, 0, 2, 3
-  ]),
-  count: 6
+  ])
 }
 
 class GameOfLife {
   constructor(w, h, cvsW = 800, cvsH = 600) {
-    this.delay = 16; //ms
+    this.delay = 32; //ms
     this.cvs = document.createElement("canvas");
     this.gl = this.cvs.getContext("webgl");
     this.cvs.width = cvsW;
@@ -52,8 +52,8 @@ class GameOfLife {
     this.h = h;
     const gl = this.gl;
     this.data = {
-      texture1: this.gl.createTexture(),
-      texture2: this.gl.createTexture(),
+      texture1: gl.createTexture(),
+      texture2: gl.createTexture(),
       swapBuffer: function () {
         const temp = this.texture1;
         this.texture1 = this.texture2;
@@ -65,7 +65,7 @@ class GameOfLife {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.bindTexture(gl.TEXTURE_2D, this.texture2);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, data);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.bindTexture(gl.TEXTURE_2D, null);
@@ -88,8 +88,6 @@ class GameOfLife {
     gl.linkProgram(this.program);
     this.locPos = gl.getAttribLocation(this.program, "position");
     this.locUv = gl.getAttribLocation(this.program, "texCoord");
-    gl.enableVertexAttribArray(this.locPos);
-    gl.enableVertexAttribArray(this.locUv);
 
     //创建VAO
     this.ext = gl.getExtension("OES_vertex_array_object");
@@ -97,15 +95,17 @@ class GameOfLife {
     this.ext.bindVertexArrayOES(this.vao);
     const posBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-    gl.vertexAttribPointer(this.locPos, 2, gl.FLOAT, false, 0, 0);
     gl.bufferData(gl.ARRAY_BUFFER, rect.positions, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(this.locPos, 2, gl.FLOAT, false, 0, 0);
     const uvBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
-    gl.vertexAttribPointer(this.uvs, 2, gl.FLOAT, false, 0, 0);
     gl.bufferData(gl.ARRAY_BUFFER, rect.uvs, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(this.locUv, 2, gl.FLOAT, false, 0, 0);
     const ebo = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, rect.indices, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(this.locPos);
+    gl.enableVertexAttribArray(this.locUv);
     this.ext.bindVertexArrayOES(null);
 
   }
@@ -119,7 +119,12 @@ class GameOfLife {
   }
 
   init() {
-
+    const data = new Uint8Array(1024 * 1024 * 4);
+    for (let i = 0; i < data.length; i++) {
+      data[i] = Math.random() * 255;
+    }
+    this.data.init(1024, 1024, data);
+    this.gl.clearColor(0, 0, 0, 1);
   }
 
   handleInput() {
@@ -131,7 +136,16 @@ class GameOfLife {
   }
 
   render() {
-
+    const gl = this.gl;
+    gl.useProgram(this.program);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.bindTexture(gl.TEXTURE_2D, this.data.getTexture());
+    this.ext.bindVertexArrayOES(this.vao);
+    gl.drawElements(gl.TRIANGLES, rect.indices.length, gl.UNSIGNED_BYTE, 0);
+    this.ext.bindVertexArrayOES(null);
+    this.data.swapBuffer();
+    gl.bindTexture(gl.TEXTURE_2D, this.data.getTexture());
+    gl.copyTexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 0, 0, this.w, this.h, 0);
   }
 
   run() {
